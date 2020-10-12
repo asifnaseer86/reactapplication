70ce1d483c38e926ca1b0bb3d05d96c475179e64
Asif Naseer <asifnaseer86@gmail.com>
2020-10-12 20:35:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   <Students CollageName='PIAIC' AIStudentName = 'Asif' OtherStudentName='All Others' />
   <hr />
   <Students CollageName='PNY' AIStudentName = 'Zubair' OtherStudentName='Nazia and Ali' />
+  <hr />
+  <Students CollageName='PNY' AIStudentName = 'Aliya' OtherStudentName='Wahab and Arham' />
   </div>
     );
     
